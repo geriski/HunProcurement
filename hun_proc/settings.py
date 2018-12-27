0d6e52541041f106ea2ac5af405185f0d3531e48
geriski <gergely.iski@gmail.com>
2018-12-27 16:38:35 +0100

    fix
    fix

--- hun_proc/settings.py
+++ hun_proc/settings.py
@@ -49,7 +49,8 @@ if os.getcwd() == '/app':
     ALLOWED_HOSTS = ['*']
 # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_URL = '/static/'
     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Application definition
