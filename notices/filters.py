eaa89a4f6c973ca975c6c80e2a479b112221f2cb
geriski <gergely.iski@gmail.com>
2018-12-28 22:47:28 +0100

    filter change
    filter change

--- notices/filters.py
+++ notices/filters.py
@@ -1,16 +1,18 @@
 import django_filters
 from .tables import RegNumberTable
 from .models import RegNumber
+from django_filters.widgets import BooleanWidget
 
 
 class RegFilter(django_filters.FilterSet):
     text = django_filters.CharFilter(label='Iktatószám', lookup_expr='icontains')
     cpv__text = django_filters.CharFilter(label='CPV-kód',lookup_expr='icontains')
+    env = django_filters.BooleanFilter(widget=BooleanWidget())
 #    nuts__text = django_filters.CharFilter(exclude = True)
     
     class Meta:
         model = RegNumber
-        fields = ['text', 'cpv__text','nuts']
+        fields = ['text', 'cpv__text','nuts',]
         
         
         
