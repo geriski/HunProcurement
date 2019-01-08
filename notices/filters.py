cbec69e5425e13b5bac58b75ab4a2613994f3182
geriski <gergely.iski@gmail.com>
2019-01-08 20:31:52 +0100

    filter updated
    filter updated

--- notices/filters.py
+++ notices/filters.py
@@ -1,13 +1,19 @@
 import django_filters
+
+
 from .tables import RegNumberTable
 from .models import RegNumber
-from django_filters.widgets import BooleanWidget
+
+STATUS_CHOICES = (
+    (0, 'Nem'),
+    (1, 'Igen'),
+    )
 
 
 class RegFilter(django_filters.FilterSet):
     text = django_filters.CharFilter(label='Iktatószám', lookup_expr='icontains')
     cpv__text = django_filters.CharFilter(label='CPV-kód',lookup_expr='icontains')
-    env = django_filters.BooleanFilter(widget=BooleanWidget())
+    env = django_filters.ChoiceFilter(empty_label="környezetvédelem",choices=STATUS_CHOICES)
 #    nuts__text = django_filters.CharFilter(exclude = True)
     
     class Meta:
